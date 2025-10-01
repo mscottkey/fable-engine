b2fcb71098be18383770f97adea131b1d01f5bfa
Scott Key <scott@driftline.ai>
2025-10-01 12:58:13 -0700

    fixed cors error

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -2,6 +2,8 @@
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+  'Access-Control-Max-Age': '86400',
 };
 
 /**
@@ -9,5 +11,8 @@ export const corsHeaders = {
  * Returns OPTIONS response with CORS headers
  */
 export function handleCorsPreflightRequest(): Response {
-  return new Response('ok', { headers: corsHeaders });
+  return new Response('ok', {
+    status: 200,
+    headers: corsHeaders
+  });
 }
